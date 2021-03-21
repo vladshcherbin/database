f2e2fb3bd4522b70ad35c58b81aaf0bd1b957c7b
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-03-21 13:22:51 +0300

    Change eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: 'airbnb-base',
-  rules: {
-    'comma-dangle': ['error', 'never'],
-    'object-curly-newline': ['error', { multiline: true, consistent: true }],
-    semi: ['error', 'never'],
-    'sort-imports': ['error', { ignoreDeclarationSort: true }],
-    'import/order': ['error', { groups: ['builtin', 'external', 'internal'], alphabetize: { order: 'asc' } }]
-  }
+  extends: '@shcherbin/eslint-config/node'
 }
