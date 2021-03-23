9f6dba23733c5f5a18e30aa500c76a938cadda3c
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-03-23 08:29:53 +0300

    Fix objection imports

--- src/connect-database.js
+++ src/connect-database.js
@@ -1,6 +1,6 @@
 import knex from 'knex'
 import logger from 'knex-query-logger'
-import { Model } from 'objection'
+import objection from 'objection'
 
 export default function connectDatabase() {
   const knexConnection = knex({
@@ -8,7 +8,7 @@ export default function connectDatabase() {
     connection: process.env.DATABASE_URL
   })
 
-  Model.knex(knexConnection)
+  objection.Model.knex(knexConnection)
 
   if (process.env.NODE_ENV === 'development') {
     logger(knexConnection)
