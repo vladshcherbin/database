ad06baab0c2277a230542e3af18151dbb250ff27
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-03-23 08:37:12 +0300

    Require knexfile in connect function

--- src/connect-database.js
+++ src/connect-database.js
@@ -2,15 +2,13 @@ import knex from 'knex'
 import logger from 'knex-query-logger'
 import objection from 'objection'
 
-export default function connectDatabase() {
-  const knexConnection = knex({
-    client: 'pg',
-    connection: process.env.DATABASE_URL
-  })
+export default function connectDatabase(knexfile) {
+  const environment = process.env.NODE_ENV || 'development'
+  const knexConnection = knex(knexfile[environment])
 
   objection.Model.knex(knexConnection)
 
-  if (process.env.NODE_ENV === 'development') {
+  if (environment === 'development') {
     logger(knexConnection)
   }
 }
