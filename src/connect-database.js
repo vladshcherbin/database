261c915d85cc1be23bf69247896cb8cc5c53a390
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-05-19 08:49:39 +0300

    Switch to direct knex config, remove env check

--- src/connect-database.js
+++ src/connect-database.js
@@ -2,13 +2,12 @@ import knex from 'knex'
 import logger from 'knex-query-logger'
 import objection from 'objection'
 
-export default function connectDatabase(knexfile) {
-  const environment = process.env.NODE_ENV || 'development'
-  const knexConnection = knex(knexfile[environment])
+export default function connectDatabase(knexConfig) {
+  const knexConnection = knex(knexConfig)
 
   objection.Model.knex(knexConnection)
 
-  if (environment === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     logger(knexConnection)
   }
 
